b8efddd26c52bb7ed8a4dc9437a9d703f814c384
kuzafkto <kuzafkto@gmail.com>
2024-04-18 13:45:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tfgproject/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.tfgproject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.tfgproject.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration= AppBarConfiguration(
+            setOf(
+                R.id.UnoFragment,
+                R.id.DosFragment
+            )
+        )
+        navView.setupWithNavController(navController)
+    }
+}
